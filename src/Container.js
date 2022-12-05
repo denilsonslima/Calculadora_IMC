6341d1e3e3a97e4dfae124238d4e83b2e078e4e4
denilsonslima <denilsondslima@gmail.com>
2022-12-04 20:41:42 -0400

    versão finalizada

--- src/Container.js
+++ src/Container.js
@@ -5,18 +5,29 @@ export default function Container() {
     const [peso, setPeso] = useState("")
     const [resultado, setResultado] = useState("Resultado")
 
-    function pesquisar(){
+    function pesquisar() {
         setPeso("")
         setInput("")
         setResultado("Resultado")
     }
 
-    function calcular(){
+    function calcular() {
         let altura = input
+        altura = altura.replace(",", ".")
         let p = peso
-        let r = (p/(altura * altura)).toFixed(2)
-        setResultado(`Seu IMC é: ${r}`)
+        let r = (p / (altura * altura)).toFixed(2)
+        if(r !== "NaN" && altura && p){
+            setResultado(`Seu IMC é: ${r}`)
+        }
     }
+
+    const tab = [
+        { descricao: "MENOR QUE 18,5", estado: "MAGREZA", valor: 0 },
+        { descricao: "ENTRE 18,5 E 24,9", estado: "NORMAL", valor: 0 },
+        { descricao: "ENTRE 25,0 E 29,9", estado: "SOBREPESO", valor: "I" },
+        { descricao: "ENTRE 30,5 E 39,0", estado: "OBESIDADE", valor: "II" },
+        { descricao: "MAIOR QUE 40,0", estado: "OBESIDADE GRAVE", valor: "III" },
+    ]
 
     return (
         <main>
@@ -24,7 +35,7 @@ export default function Container() {
             <div>
                 <div>
                     <h2>Altura</h2>
-                    <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+                    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
                     <br />
                     <button onClick={pesquisar}>Limpar</button>
                 </div>
@@ -35,7 +46,34 @@ export default function Container() {
                     <button onClick={calcular}>Calcular</button>
                 </div>
             </div>
-            <div>{resultado}</div>
+            <div className="resultado">{resultado}</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>IMC</th>
+                        <th>Classificação</th>
+                        <th>Grau</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tab.map((p, i) =>
+                        <Tabela
+                            key={i}
+                            descricao={p.descricao}
+                            estado={p.estado}
+                            valor={p.valor}
+                        />)}
+                </tbody>
+            </table>
         </main>
-    )   
+    )
+}
+function Tabela({ descricao, estado, valor}) {
+    return (
+        <tr bgcolor={""}>
+            <td>{descricao}</td>
+            <td>{estado}</td>
+            <td>{valor}</td>
+        </tr>
+    )
 }
