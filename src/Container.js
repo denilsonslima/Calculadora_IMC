13d52bfb93401ae8fac9322ad17d227429cd9001
denilsonslima <denilsondslima@gmail.com>
2022-12-09 22:17:44 -0400

    projeto finalizado

--- src/Container.js
+++ src/Container.js
@@ -4,6 +4,7 @@ export default function Container() {
     const [input, setInput] = useState("")
     const [peso, setPeso] = useState("")
     const [resultado, setResultado] = useState("Resultado")
+    const [valorImc, setFaixa] = useState(0)
 
     function pesquisar() {
         setPeso("")
@@ -16,17 +17,33 @@ export default function Container() {
         altura = altura.replace(",", ".")
         let p = peso
         let r = (p / (altura * altura)).toFixed(2)
-        if(r !== "NaN" && altura && p){
+        if (r !== "NaN" && altura && p) {
             setResultado(`Seu IMC é: ${r}`)
+            selecionarFaixa(Number(r))
         }
     }
 
+
+    function selecionarFaixa(calculoImc) {
+        if (calculoImc <= 18.5) {
+          setFaixa(1);
+        } else if (calculoImc > 18.5 && calculoImc <= 24.9) {
+          setFaixa(2);
+        } else if (calculoImc > 24.9 && calculoImc <= 29.9) {
+          setFaixa(3);
+        } else if (calculoImc > 29.9 && calculoImc <= 39.9) {
+          setFaixa(4);
+        } else if (calculoImc > 39.9) {
+          setFaixa(5);
+        }
+      }
+
     const tab = [
-        { descricao: "MENOR QUE 18,5", estado: "MAGREZA", valor: 0 },
-        { descricao: "ENTRE 18,5 E 24,9", estado: "NORMAL", valor: 0 },
-        { descricao: "ENTRE 25,0 E 29,9", estado: "SOBREPESO", valor: "I" },
-        { descricao: "ENTRE 30,5 E 39,0", estado: "OBESIDADE", valor: "II" },
-        { descricao: "MAIOR QUE 40,0", estado: "OBESIDADE GRAVE", valor: "III" },
+        { id: 1, descricao: "MENOR QUE 18,5", estado: "MAGREZA", valor: 0 },
+        { id: 2, descricao: "ENTRE 18,5 E 24,9", estado: "NORMAL", valor: 0 },
+        { id: 3, descricao: "ENTRE 25,0 E 29,9", estado: "SOBREPESO", valor: "I" },
+        { id: 4, descricao: "ENTRE 30,5 E 39,0", estado: "OBESIDADE", valor: "II" },
+        { id: 5, descricao: "MAIOR QUE 40,0", estado: "OBESIDADE GRAVE", valor: "III" },
     ]
 
     return (
@@ -56,24 +73,15 @@ export default function Container() {
                     </tr>
                 </thead>
                 <tbody>
-                    {tab.map((p, i) =>
-                        <Tabela
-                            key={i}
-                            descricao={p.descricao}
-                            estado={p.estado}
-                            valor={p.valor}
-                        />)}
+                    {tab.map((p) =>
+                        <tr key={p.id} bgcolor={valorImc === p.id ? "#CDEAE3" : "#ffffff"}>
+                            <td>{p.descricao}</td>
+                            <td>{p.estado}</td>
+                            <td>{p.valor}</td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </main>
-    )
-}
-function Tabela({ descricao, estado, valor}) {
-    return (
-        <tr bgcolor={""}>
-            <td>{descricao}</td>
-            <td>{estado}</td>
-            <td>{valor}</td>
-        </tr>
     )
 }
